e1f04beedfd16f171ddd3f1264e5032444eca2fa
Mani Movassagh <mani.mgh@gmail.com>
2022-09-19 22:19:24 +0200

    add post and get all products feature for product service

--- product-service/src/main/java/com/fithub/manimovassagh/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/fithub/manimovassagh/productservice/controller/ProductController.java
@@ -2,11 +2,14 @@ package com.fithub.manimovassagh.productservice.controller;
 
 
 import com.fithub.manimovassagh.productservice.dto.ProductRequest;
+import com.fithub.manimovassagh.productservice.dto.ProductResponse;
 import com.fithub.manimovassagh.productservice.model.Product;
 import com.fithub.manimovassagh.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -19,7 +22,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest);
+    }
 
-
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+       return  productService.getAllProducts();
     }
 }
--- product-service/src/main/java/com/fithub/manimovassagh/productservice/service/ProductService.java
+++ product-service/src/main/java/com/fithub/manimovassagh/productservice/service/ProductService.java
@@ -2,11 +2,15 @@ package com.fithub.manimovassagh.productservice.service;
 
 
 import com.fithub.manimovassagh.productservice.dto.ProductRequest;
+import com.fithub.manimovassagh.productservice.dto.ProductResponse;
 import com.fithub.manimovassagh.productservice.model.Product;
 import com.fithub.manimovassagh.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,7 +26,19 @@ public class ProductService {
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice()).build();
         productRepository.save(product);
-        log.info("Product {} Successfully created",product.getId());
+        log.info("Product {} Successfully created", product.getId());
     }
 
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+         return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder().id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
 }
