50bef4cd0e590babd11d0cb1e1ac8bf10ed34d7b
Mani Movassagh <mani.mgh@gmail.com>
2022-09-23 12:41:21 +0200

    order service work and smoked tested

--- order-service/src/main/java/com/github/manimovassagh/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/github/manimovassagh/orderservice/controller/OrderController.java
@@ -4,10 +4,8 @@ package com.github.manimovassagh.orderservice.controller;
 import com.github.manimovassagh.orderservice.dto.OrderRequest;
 import com.github.manimovassagh.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
@@ -16,6 +14,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "order Placed successfully";
